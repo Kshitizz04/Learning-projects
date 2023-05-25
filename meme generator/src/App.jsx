ab834fd426fdaa2a27aad50d6c95c5e472439adf
Kshitizz04 <107020408+Kshitizz04@users.noreply.github.com>
2023-05-26 00:31:19 +0530

    Meme generator from Scrimba's free react course
    Doesn't include node modules

---
+++ meme generator/src/App.jsx
@@ -0,0 +1,12 @@
+import './Style.css'
+import Header from "./Header.jsx"
+import Meme from "./Meme.jsx"
+
+export default function App() {
+  return (
+      <div className="app">
+          <Header />
+          <Meme />
+      </div>
+  )
+}
